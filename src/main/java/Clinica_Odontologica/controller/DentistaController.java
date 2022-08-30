ff2beb5f28308f06105954e7ff8e8277f12f78e6
dougsn <douglassnascimet@gmail.com>
2022-08-30 20:50:12 -0300

    Finalizando o CRUD da entidade Dentista

--- src/main/java/Clinica_Odontologica/controller/DentistaController.java
+++ src/main/java/Clinica_Odontologica/controller/DentistaController.java
@@ -4,9 +4,12 @@ package Clinica_Odontologica.controller;
 import Clinica_Odontologica.model.Dentista;
 import Clinica_Odontologica.service.DentistaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,28 +21,75 @@ public class DentistaController {
     DentistaService dentistaService;
 
     @PostMapping
-    public Dentista salvar(@RequestBody Dentista dentista) throws SQLException{
-        return dentistaService.salvar(dentista);
+    public ResponseEntity<Dentista> salvar(@RequestBody Dentista dentista) throws SQLException{
+
+      ResponseEntity responseEntity = null;
+
+      Dentista dentista1 = dentistaService.salvar(dentista);
+
+        if(dentista1.getNome() == null || dentista1.getMatricula() == null || dentista1.getSobrenome() == null){
+            responseEntity = new ResponseEntity(HttpStatus.NOT_FOUND);
+        } else {
+            responseEntity = new ResponseEntity(dentista1,HttpStatus.OK);
+        }
+        return responseEntity;
     }
 
     @GetMapping
-    public List<Dentista> buscarTodos() throws SQLException {
-        return dentistaService.buscarTodos();
+    public ResponseEntity<List<Dentista>>  buscarTodos() throws SQLException {
+
+        ResponseEntity responseEntity = null;
+
+        List<Dentista> dentistas;
+        dentistas = dentistaService.buscarTodos();
+
+        if(dentistas.size() == 0){
+            responseEntity = new ResponseEntity(HttpStatus.NOT_FOUND);
+        } else {
+            responseEntity = new ResponseEntity(dentistas,HttpStatus.OK);
+        }
+        return responseEntity;
+
     }
 
     @GetMapping("/buscar/{id}")
-    public Dentista buscarPorId(@PathVariable int id) throws SQLException {
-        return dentistaService.buscarPorId(id);
+    public ResponseEntity<Dentista> buscarPorId(@PathVariable int id) throws SQLException {
+
+        ResponseEntity responseEntity = null;
+
+        if(dentistaService.buscarPorId(id)==null){
+            responseEntity = new ResponseEntity(HttpStatus.NOT_FOUND);
+        } else {
+            responseEntity = new ResponseEntity(dentistaService.buscarPorId(id), HttpStatus.OK);
+        }
+        return responseEntity;
+
     }
 
     @PutMapping("/alterar")
-    public String alterar(@RequestBody Dentista dentista) throws SQLException{
-        return dentistaService.alterar(dentista);
+    public ResponseEntity<Dentista> alterar(@RequestBody Dentista dentista) throws SQLException{
+
+        ResponseEntity responseEntity = null;
+
+        if(dentistaService.buscarPorId(dentista.getId())==null){
+            responseEntity = new ResponseEntity(HttpStatus.NOT_FOUND);
+        } else {
+            responseEntity = new ResponseEntity(dentistaService.alterar(dentista), HttpStatus.OK);
+        }
+        return responseEntity;
+
     }
 
     @DeleteMapping("/excluir/{id}")
-    public String excluir(@PathVariable int id) throws SQLException {
-        return dentistaService.excluir(id);
+    public ResponseEntity excluir(@PathVariable int id) throws SQLException {
+        ResponseEntity responseEntity = null;
+
+        if(dentistaService.buscarPorId(id)==null){
+            responseEntity = new ResponseEntity(HttpStatus.NOT_FOUND);
+        } else {
+            responseEntity = new ResponseEntity(dentistaService.excluir(id), HttpStatus.OK);
+        }
+        return responseEntity;
     }
 
 
